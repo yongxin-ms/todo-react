277e86c00f24f9d5ed1c9b74ae82db73f582ce58
Will <will@i007.cc>
2023-08-19 18:32:55 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,18 +1,45 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [todos, setTodos] = useState([
+    "Read a book",
+    "Write a blog",
+    "Make a video",
+    "Reply a comment",
+  ]);
+
+  const [todo, setTodo] = useState("");
+
+  const handleClick = () => {
+    if (!todo) {
+      return;
+    }
+
+    const newTodos = todos.concat([todo]);
+    setTodos(newTodos);
+    setTodo("");
+  };
+
+  const handleChange = (e) => {
+    const todo = e.target.value;
+    setTodo(todo);
+    console.log(e.target.value);
+    console.log({ todo });
+  };
+
   return (
     <div className="App">
       <h1>My Todo List</h1>
       <div>
-        <input type="text" />
-        <button>Add</button>
+        <input type="text" onChange={handleChange} />
+        <button onClick={handleClick}>Add</button>
       </div>
       <ul>
-        <li>Read a book</li>
-        <li>Write a blog</li>
-        <li>Make a video</li>
+        {todos.map((todo) => {
+          return <li>{todo}</li>;
+        })}
       </ul>
     </div>
   );
