00dbae0f11fb45dceb37f1fe3adf868e051aa688
Will <will@i007.cc>
2023-08-19 17:59:34 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>My Todo List</h1>
+      <div>
+        <input type="text" />
+        <button>Add</button>
+      </div>
+      <ul>
+        <li>Read a book</li>
+        <li>Write a blog</li>
+        <li>Make a video</li>
+      </ul>
     </div>
   );
 }
